83d702e6e9a3082e967a99b750ffdaed32e00a98
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-11-14 23:52:25 +0700

    thêm loading state và optimize quá trình hiển thị comment trên bottom sheet

--- video_streaming_app_ui/src/component/comment/CommentBottomSheet.tsx
+++ video_streaming_app_ui/src/component/comment/CommentBottomSheet.tsx
@@ -1,11 +1,11 @@
-import React, {createContext, useCallback, useContext, useMemo, useRef} from 'react';
+import React, {forwardRef, useCallback, useImperativeHandle, useMemo, useRef, useState} from 'react';
 import BottomSheet, {
   BottomSheetFlatList,
 } from '@gorhom/bottom-sheet';
 import {FlatList, Image, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
-import data from '../../data/comments.json';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import LottieView from 'lottie-react-native';
 
 interface UserProfile {
   username: string;
@@ -19,9 +19,13 @@ interface Comment {
   reply?: Comment[]
 }
 
-const comments : Comment[] = data;
+const Comment = React.memo(({ comment }: { comment: Comment }) => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
-function Comment({comment} : {comment: Comment}) : React.JSX.Element {
+  const separator = useCallback(() => {
+    return <View style={styles.separator} />;
+  }, []);
+
   const renderItem = useCallback(({ item }: { item: Comment }) => {
     return <Comment comment={item} />;
   }, []);
@@ -55,96 +59,130 @@ function Comment({comment} : {comment: Comment}) : React.JSX.Element {
         </View>
         {comment.reply &&
           <View style={{paddingHorizontal: 10, paddingVertical: 20}}>
-          <FlatList data={comment.reply}
+          {!isExpanded ?
+            <TouchableOpacity onPress={() => setIsExpanded(true)}>
+              <Text style={{fontWeight: 'bold', color: 'gray'}}>Show more reply ({comment.reply.length})</Text>
+            </TouchableOpacity> :
+            <FlatList data={comment.reply}
             ItemSeparatorComponent={separator}
             renderItem={renderItem}
             keyExtractor={(item, index) => index.toString()}
           />
+          }
           </View>
         }
       </View>
     </View>
   );
+});
+
+export interface CommentSheet {
+  close: () => void;
+  open: () => void;
 }
 
-// Tạo Context
-const BottomSheetContext = createContext<{
-  openBottomSheet: () => void;
-} | null>(null);
-
-export const useBottomSheet = () => useContext(BottomSheetContext);
-
-const separator = () : React.JSX.Element => {
-  return <View style={styles.separator} />;
-};
-
-export default function CommentBottomSheet({children} : {children: any}) : React.JSX.Element {
-    const renderItem = useCallback(({ item }: { item: Comment }) => {
-      return <Comment comment={item} />;
-    }, []);
-
-    // ref
-    const bottomSheetRef = useRef<BottomSheet>(null);
-
-    const snapPoints : string[] = useMemo<string[]>(() => ['50%', '75%'], []);
-
-    const openBottomSheet = useCallback(() => {
-      bottomSheetRef.current?.snapToIndex(0);
-    }, []);
-
-    const handleSheetChanges = useCallback((index: number) => {
-      console.log('handleSheetChanges', index);
-    }, []);
-    // renders
-    return (
-        <BottomSheetContext.Provider value={{openBottomSheet}}>
-          {children}
-          <BottomSheet
-          ref={bottomSheetRef}
-          snapPoints={snapPoints}
-          onChange={handleSheetChanges}
-          enableDynamicSizing={false}
-          handleIndicatorStyle={{backgroundColor: 'white'}}
-          handleStyle={{backgroundColor: 'black'}}
-          enablePanDownToClose={true}
-          >
-            <BottomSheetFlatList
-            data={comments}
-            renderItem={renderItem}
-            ItemSeparatorComponent={separator}
-            contentContainerStyle={{
-              padding: 10,
-            }}
-            keyExtractor={(item, index) => index.toString()}
-            style={styles.contentContainer}
-            />
-            <View style={{
-              paddingHorizontal: 10,
-              backgroundColor: 'black',
-              flexDirection: 'row',
-              gap: 10,
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              }}
-            >
-              <TextInput
-                placeholderTextColor={'gray'}
-                cursorColor={'green'}
-                numberOfLines={1}
-                selectionColor={'green'}
-                placeholder="Nhập bình luận"
-              />
-              <TouchableOpacity>
-                <FeatherIcon name="send" style={{
-                  color: 'lightgray',
-                  fontSize: 24,
-                }} />
-              </TouchableOpacity>
-            </View>
-          </BottomSheet>
-        </BottomSheetContext.Provider>
-    );
+interface CommentBottomSheetProps {
+  comments: Comment[];
 }
+
+const CommentBottomSheet = forwardRef<CommentSheet, CommentBottomSheetProps>(({comments} , ref) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+
+  const renderItem = useCallback(({ item }: { item: Comment }) => {
+    return <Comment comment={item} />;
+  }, []);
+
+  const separator = useCallback(() => {
+    return <View style={styles.separator} />;
+  }, []);
+
+  // ref
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const snapPoints : string[] = useMemo<string[]>(() => ['50%', '75%'], []);
+
+  useImperativeHandle(ref, () => ({
+    open: () => {
+      setIsVisible(true);
+      setIsLoading(true);
+      bottomSheetRef.current?.expand();
+      setTimeout(() => setIsLoading(false), 1000);
+    },
+    close: () => {
+      setIsVisible(false);
+      bottomSheetRef.current?.close();
+    },
+  }));
+
+  // renders
+  return (
+    isVisible && <BottomSheet
+    ref={bottomSheetRef}
+    snapPoints={snapPoints}
+    enableDynamicSizing={false}
+    handleIndicatorStyle={{backgroundColor: 'white'}}
+    handleStyle={{backgroundColor: 'black'}}
+    enablePanDownToClose={true}
+    onClose={() => setIsVisible(false)}
+    >
+    {isLoading ? (
+      <View style={{backgroundColor: 'black', alignItems: 'center', justifyContent: 'center', flex: 1}}>
+        <LottieView style={{width: 50, height: 50}}
+                    source={require('../../assest/loading.json')}
+                    autoPlay loop
+        />
+      </View>
+    ) : (
+    <>
+      <BottomSheetFlatList
+      data={comments}
+      renderItem={renderItem}
+      ItemSeparatorComponent={separator}
+      contentContainerStyle={{
+        padding: 10,
+      }}
+      keyExtractor={(item, index) => index.toString()}
+      style={styles.contentContainer}
+      initialNumToRender={0}
+      />
+      <View style={{
+        paddingHorizontal: 10,
+        backgroundColor: 'black',
+        flexDirection: 'row',
+        alignItems: 'center',
+        position: 'relative',
+        }}
+      >
+        <TextInput
+          style={{color: 'white', width: '80%'}}
+          placeholderTextColor={'gray'}
+          cursorColor={'green'}
+          numberOfLines={1}
+          selectionColor={'green'}
+          placeholder="Enter your comment"
+        />
+        <TouchableOpacity style={{
+            position: 'absolute',
+            right: 10,
+            top: '50%',
+            transform: [
+              { translateY: '-50%' },
+            ],
+          }}>
+          <FeatherIcon name="send" style={{
+            color: 'lightgray',
+            fontSize: 24,
+          }} />
+        </TouchableOpacity>
+      </View>
+    </>
+  )}
+    </BottomSheet>
+  );
+});
+
 
 const styles = StyleSheet.create({
   contentContainer: {
@@ -172,3 +210,5 @@ const styles = StyleSheet.create({
     height: '100%',
   },
 });
+
+export default CommentBottomSheet;
