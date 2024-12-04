6e75f8034c5e1cac91a0407642e3c8504836fa73
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-12-04 18:24:49 +0700

    hoàn thành section 'bài viết đã đăng'

--- video_streaming_app_ui/src/screen/ProfileScreen.tsx
+++ video_streaming_app_ui/src/screen/ProfileScreen.tsx
@@ -1,8 +1,19 @@
-import {Image, ImageStyle, SafeAreaView, StyleSheet, TextStyle, TouchableOpacity, View, ViewStyle} from 'react-native';
-import React, { useMemo } from 'react';
+import {FlatList, Image, ImageStyle, SafeAreaView, StyleSheet, TextStyle, TouchableOpacity, View, ViewStyle} from 'react-native';
+import React, { useCallback, useMemo } from 'react';
 import { Text } from 'react-native';
 import IconIonicons from 'react-native-vector-icons/Ionicons';
 import IconEntypo from 'react-native-vector-icons/Entypo';
+import posted_posts from '../data/user-profile-posted.json';
+import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
+
+type Post = {
+  image: string,
+  likes: number,
+  view: number,
+  comments: number,
+  description: string,
+}
+const data : Post[] = posted_posts.posts;
 
 const profileStyle : ViewStyle = {
   flexDirection: 'row',
@@ -50,100 +61,130 @@ const ProfileHeader = () : React.JSX.Element => {
   );
 };
 
-const infoWrapper : ViewStyle = {
-  flexDirection: 'row',
-  gap: 20,
-  alignItems: 'center',
-  justifyContent: 'center',
-};
-type AvatarProfileStyle = {
-  imgBox: ViewStyle,
-  img: ImageStyle,
-  container: ViewStyle,
-  userFullName: TextStyle,
-};
-const avatarProfileWrapper : AvatarProfileStyle = {
-  container: {
-    gap: 5,
-    alignItems: 'center',
-    flexDirection: 'column',
-  },
-  imgBox: {
-    width: 70,
-    height: 70,
-    overflow: 'hidden',
-    borderRadius: 50,
-    borderWidth: 2,
-    borderColor: 'white',
-  },
-  img: {
-    width: '100%',
-    height: '100%',
-  },
-  userFullName: {
-    color: 'white',
-    fontSize: 15,
-    fontWeight: 'bold',
-  },
-};
-type StatOverallStyle = {
-  container: ViewStyle,
-  topText: TextStyle,
-  bottomText: TextStyle,
-}
-const statOverallStyle : StatOverallStyle = {
-  container: {
-    flexDirection: 'row',
-    gap: 10,
-  },
-  topText:  {
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  bottomText: {
-    fontSize: 15,
-    color: 'white',
-  },
-};
-const ProfileInfo = () : React.JSX.Element => {
+const ProfileBody = ({children, style} : {children: React.ReactNode, style?: ViewStyle}) : React.JSX.Element => {
   return (
-    <View style={infoWrapper}>
-      <View style={[avatarProfileWrapper.container]}>
-        <TouchableOpacity style={[avatarProfileWrapper.imgBox]}>
-          <Image style={[avatarProfileWrapper.img]}
-          source={{uri: 'https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x4.jpg'}}
-          />
-        </TouchableOpacity>
-        <Text style={[avatarProfileWrapper.userFullName]}>Nguyễn Tuấn</Text>
+    <View style={[style]}>
+      {children}
+    </View>
+  );
+};
+
+const ProfileInfo = () : React.JSX.Element => {
+  type AvatarProfileStyle = {
+    imgBox: ViewStyle,
+    img: ImageStyle,
+    container: ViewStyle,
+    userFullName: TextStyle,
+  };
+  type StatOverallStyle = {
+    container: ViewStyle,
+    topText: TextStyle,
+    bottomText: TextStyle,
+  }
+  type ProfileDescriptionStyle = {
+    wrapper: ViewStyle,
+    descriptionText: TextStyle
+  }
+
+  const avatarProfileStyle = useMemo<AvatarProfileStyle>(() => ({
+    container: {
+      gap: 5,
+      flexDirection: 'column',
+    },
+    imgBox: {
+      width: 70,
+      height: 70,
+      overflow: 'hidden',
+      borderRadius: 50,
+      borderWidth: 2,
+      borderColor: 'white',
+    },
+    img: {
+      width: '100%',
+      height: '100%',
+    },
+    userFullName: {
+      color: 'white',
+      fontSize: 15,
+      fontWeight: 'bold',
+    },
+  }), []);
+
+  const statOverallStyle = useMemo<StatOverallStyle>(() => ({
+    container: {
+      flexDirection: 'row',
+      gap: 10,
+    },
+    topText:  {
+      textAlign: 'center',
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+    bottomText: {
+      fontSize: 15,
+      color: 'white',
+    },
+  }), []);
+
+  const headerInfoStyle = useMemo<ViewStyle>(() => ({
+    flexDirection: 'row',
+    gap: 20,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  }), []);
+
+  const descriptionStyle = useMemo<ProfileDescriptionStyle>(() => ({
+    descriptionText: {
+      color: 'white'
+    },
+    wrapper: {
+      maxWidth: '80%',
+    },
+  }), []);
+
+  return (
+    <View>
+      <View style={headerInfoStyle}>
+        <View style={[avatarProfileStyle.container]}>
+          <TouchableOpacity style={[avatarProfileStyle.imgBox]}>
+            <Image style={[avatarProfileStyle.img]}
+            source={{uri: 'https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x4.jpg'}}
+            />
+          </TouchableOpacity>
+          <Text style={[avatarProfileStyle.userFullName]}>Nguyễn Tuấn</Text>
+        </View>
+        <View style={[statOverallStyle.container]}>
+          <View>
+            <Text style={[statOverallStyle.topText]}>0</Text>
+            <Text style={[statOverallStyle.bottomText]}>bài viết</Text>
+          </View>
+          <View>
+            <Text style={[statOverallStyle.topText]}>2</Text>
+            <Text style={[statOverallStyle.bottomText]}>Người theo dõi</Text>
+          </View>
+          <View>
+            <Text style={[statOverallStyle.topText]}>16</Text>
+            <Text style={[statOverallStyle.bottomText]}>Đang theo dõi</Text>
+          </View>
+        </View>
       </View>
-      <View style={[statOverallStyle.container]}>
-        <View>
-          <Text style={[statOverallStyle.topText]}>0</Text>
-          <Text style={[statOverallStyle.bottomText]}>bài viết</Text>
-        </View>
-        <View>
-          <Text style={[statOverallStyle.topText]}>2</Text>
-          <Text style={[statOverallStyle.bottomText]}>Người theo dõi</Text>
-        </View>
-        <View>
-          <Text style={[statOverallStyle.topText]}>16</Text>
-          <Text style={[statOverallStyle.bottomText]}>Đang theo dõi</Text>
-        </View>
+      <View style={[descriptionStyle.wrapper]}>
+        <Text style={[descriptionStyle.descriptionText]}>Tôi là sinh viên trường đại học Nông Lâm Thành Phố Hồ Chí Minh 🐧</Text>
       </View>
     </View>
   );
 };
 
 
-type ProfileActions_Style = {
-  container: ViewStyle,
-  actionButton: ViewStyle,
-  actionText: TextStyle,
-}
 const ProfileActions = () : React.JSX.Element => {
-  const style = useMemo<ProfileActions_Style>(() => ({
+  type ProfileActions_Style = {
+    container: ViewStyle,
+    actionButton: ViewStyle,
+    actionText: TextStyle,
+  }
+
+  const {container, actionButton, actionText} = useMemo<ProfileActions_Style>(() => ({
     container: {
       flexDirection: 'row',
       gap: 5,
@@ -160,13 +201,132 @@ const ProfileActions = () : React.JSX.Element => {
     },
   }), []);
   return (
-    <View style={[style.container]}>
-      <TouchableOpacity style={style.actionButton}>
-        <Text style={style.actionText}>Chỉnh sửa trang cá nhân</Text>
+    <View style={[container]}>
+      <TouchableOpacity style={actionButton}>
+        <Text style={actionText}>Chỉnh sửa trang cá nhân</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={style.actionButton}>
+      <TouchableOpacity style={actionButton}>
         <IconIonicons name="person-add-sharp" color={'white'} size={15}/>
       </TouchableOpacity>
+    </View>
+  );
+};
+
+const ProfilePostArchived = () : React.JSX.Element => {
+  return (
+    <View>
+
+    </View>
+  )
+}
+
+
+const PostCard = ({post} : {post : Post}) : React.JSX.Element => {
+  type PostCardStyle = {
+    container: ViewStyle,
+    img_wrapper: ViewStyle,
+    post_img: ImageStyle,
+    icon: TextStyle,
+    text: TextStyle,
+    info_wrapper: ViewStyle,
+    info_stat: ViewStyle,
+  }
+  const style = useMemo<PostCardStyle>(() => ({
+    container: {
+      position: 'relative',
+      borderWidth: 2,
+      borderColor: '#333333',
+      width: 130,
+      height: 200,
+    },
+    img_wrapper: {
+
+    },
+    post_img: {
+      width: '100%',
+      resizeMode: 'cover',
+      height: '100%',
+    },
+    info_wrapper: {
+      backgroundColor: 'rgba(0,0,0,.8)',
+      position: 'absolute',
+      left: 0,
+      bottom: 0,
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 5,
+      width: '100%',
+      gap: 15,
+    },
+    info_stat: {
+      flexDirection: 'row',
+      gap: 5,
+      alignItems: 'center',
+    },
+    icon: {
+      fontSize: 13,
+      color: 'white',
+    },
+    text: {
+      fontSize: 10,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+  }), []);
+  return (
+    <TouchableOpacity style={[style.container]}>
+      <View style={[style.img_wrapper]}>
+        <Image source={{uri: post.image}} style={[style.post_img]}/>
+      </View>
+      <View style={[style.info_wrapper]}>
+        <View style={[style.info_stat]}>
+          <IconFontAwesome name="eye" style={[style.icon]}/>
+          <Text style={[style.text]}>{post.view}</Text>
+        </View>
+        <View style={[style.info_stat]}>
+          <IconFontAwesome name="comment-o" style={[style.icon]}/>
+          <Text style={[style.text]}>{post.comments}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const ProfilePostedPost = () : React.JSX.Element => {
+  type ProfilePostedPostStyle = {
+    container: ViewStyle,
+    title_wrapper: ViewStyle,
+    title_text: TextStyle,
+    title_icon: TextStyle,
+  }
+  const style = useMemo<ProfilePostedPostStyle>(() => ({
+    container: {
+
+    },
+    title_wrapper: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      gap: 10,
+    },
+    title_text: {
+      color: 'white',
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    title_icon: {
+      color: 'white',
+      fontSize: 20,
+    },
+  }), []);
+  return (
+    <View style={[style.container]}>
+      <View style={[style.title_wrapper]}>
+        <IconEntypo name="folder-video" style={[style.title_icon]}/>
+        <Text style={[style.title_text]}>Bài viết đã đăng tải</Text>
+      </View>
+      <FlatList data={data} renderItem={({item : post}) => {
+        return <PostCard post={post}/>;
+      }}/>
     </View>
   );
 };
@@ -177,6 +337,10 @@ export default function ProfileScreen():React.ReactElement {
       <ProfileHeader/>
       <ProfileInfo/>
       <ProfileActions/>
+      <ProfileBody>
+        <ProfilePostArchived/>
+        <ProfilePostedPost/>
+      </ProfileBody>
     </SafeAreaView>
   );
 }
