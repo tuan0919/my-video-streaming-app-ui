16197725650da3ee7ff5ee707caeaf94772cbb17
tuan0919 <21130601@st.hcmuaf.edu.vn>
2024-12-05 21:34:37 +0700

    update import

--- video_streaming_app_ui/src/component/index.ts
+++ video_streaming_app_ui/src/component/index.ts
@@ -3,11 +3,12 @@ import Stories from './home/Stories';
 import AppLogo from './misc/AppLogo';
 import NewFeed from './home/NewFeed.tsx';
 import CommentBottomSheet from './comment/CommentBottomSheet.tsx';
-
+import ExploreFriends from './profile/ExploreFriends.tsx';
 export {
   Header,
   Stories,
   AppLogo,
   NewFeed,
   CommentBottomSheet,
+  ExploreFriends,
 };
